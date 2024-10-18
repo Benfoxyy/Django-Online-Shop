c1db65f82aa5010e8cb70e290e60ec95a06ddc9e
Ben <146076866+Benfoxyy@users.noreply.github.com>
2024-10-18 11:13:10 +0330

    plus and minus in prod detail

--- core/cart/cart.py
+++ core/cart/cart.py
@@ -22,7 +22,7 @@ class CartSession:
         else:
             new_prod = {
                 'product_id': product_id,
-                'quantity': 1,
+                'quantity': 1 if quantity is None else int(quantity),
             }
             self.cart['items'].append(new_prod)
         self.save()
@@ -47,15 +47,15 @@ class CartSession:
         return self.cart['items']
     
     def get_cart_quantity(self):
-        return sum(item['quantity'] for item in self.cart['items'])
+        return sum(int(item['quantity']) for item in self.cart['items'])
     
     def get_total_price(self):
         items = self.get_cart_items()
         for item in items:
             if not item.get('prod_obj').discount_percent:
-                self.cart['total_price'] += item.get('prod_obj').price * item.get('quantity')
+                self.cart['total_price'] += item.get('prod_obj').price * int(item.get('quantity'))
             else:
-                self.cart['total_price'] += item.get('prod_obj').offer() * item.get('quantity')
+                self.cart['total_price'] += item.get('prod_obj').offer() * int(item.get('quantity'))
         tax = self.cart['total_price'] * Decimal('0.09')
         
         return int(self.cart['total_price'] + tax)
