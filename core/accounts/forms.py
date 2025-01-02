3eeba41eecb3e9d19a3fa87cdc36b3d7967ef9df
Ben <146076866+Benfoxyy@users.noreply.github.com>
2025-01-02 11:46:39 +0330

    reset password customize

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -1,19 +1,65 @@
 from django import forms
-from django.contrib.auth import forms as auth_forms
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import password_validation
 from django.core.exceptions import ValidationError
 from .models import User
+from django.utils.translation import gettext_lazy as _
 
 
-class RegistrationForm(auth_forms.UserCreationForm):
+class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     class Meta:
         model = User
         fields = ['email', 'password1', 'password2']
 
 
-class AuthenticationForm(auth_forms.AuthenticationForm):
+class AuthenticationForm(AuthenticationForm):
     def confirm_login_allowed(self, user):
         super(AuthenticationForm,self).confirm_login_allowed(user)
 
         # if not user.is_verified:
         #     raise ValidationError("user is not verified")
+
+
+class SetPasswordForm(forms.Form):
+    """
+    A form that lets a user set their password without entering the old
+    password
+    """
+
+    error_messages = {
+        "password_mismatch": _("The two password fields didn’t match."),
+    }
+    new_password1 = forms.CharField(
+        # label=_("New password"),
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password","class":"form-control"}),
+        strip=False,
+        # help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        # label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password","class":"form-control"}),
+    )
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super().__init__(*args, **kwargs)
+
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get("new_password1")
+        password2 = self.cleaned_data.get("new_password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages["password_mismatch"],
+                code="password_mismatch",
+            )
+        password_validation.validate_password(password2, self.user)
+        return password2
+
+    def save(self, commit=True):
+        password = self.cleaned_data["new_password1"]
+        self.user.set_password(password)
+        if commit:
+            self.user.save()
+        return self.user
--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from.forms import SetPasswordForm
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='sign-up'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/reset_password/reset_password.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/reset_password/reset_password_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/reset_password/reset_password_form.html',
+        form_class = SetPasswordForm
+        ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/reset_password/reset_password_complete.html'), name='password_reset_complete'),
 ]
